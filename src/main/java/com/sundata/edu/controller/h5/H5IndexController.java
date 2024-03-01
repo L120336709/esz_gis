810062769c054f6a3203e9507913e31a193a7e66
李继刚 <120336709@qq.com>
2024-03-01 15:17:29 +0800

    小程序导航，增加h5跳转到小程序页面，直接唤起手机导航app

--- src/main/java/com/sundata/edu/controller/h5/H5IndexController.java
+++ src/main/java/com/sundata/edu/controller/h5/H5IndexController.java
@@ -31,7 +31,7 @@ import java.util.List;
  * @Create: 2021-11-12 13:04
  **/
 @Controller
-public class H5IndexController extends BaseController{
+public class H5IndexController extends BaseController {
 
     @Autowired
     private RpcLoginService rpcLoginService;
@@ -39,7 +39,7 @@ public class H5IndexController extends BaseController{
     @Autowired
     private ITokenService iTokenService;
 
-     @Autowired
+    @Autowired
     private SchedulesService schedulesService;
 
 
@@ -48,100 +48,92 @@ public class H5IndexController extends BaseController{
 
     @GetMapping(value = "/h5/index")
     public ModelAndView index(String accessToken, ModelMap map) {
-        System.err.println("accessToken00="+accessToken);
-        accessToken=iTokenService.getWebTokenByAppToken(accessToken).getData();
-        System.err.println("accessToken01="+accessToken);
-        if(StringUtils.isBlank(accessToken)) {
+        System.err.println("accessToken00=" + accessToken);
+        accessToken = iTokenService.getWebTokenByAppToken(accessToken).getData();
+        System.err.println("accessToken01=" + accessToken);
+        if (StringUtils.isBlank(accessToken)) {
             return new ModelAndView(prefix + "/error");
         }
         rpcLoginService.login(accessToken);
-        ModelAndView view = new ModelAndView(prefix+"/index", map);
+        ModelAndView view = new ModelAndView(prefix + "/index", map);
         return view;
     }
-
 
 
     @GetMapping(value = "/h5/search")
     public String search() {
-        return prefix+"/search";
+        return prefix + "/search";
     }
 
     @GetMapping(value = "/h5/detail")
-    public ModelAndView detail(String peopleid,ModelMap map) {
-        map.addAttribute("peopleid",peopleid);
-        ModelAndView view = new ModelAndView( prefix+"/detail",map);
+    public ModelAndView detail(String peopleid, ModelMap map) {
+        map.addAttribute("peopleid", peopleid);
+        ModelAndView view = new ModelAndView(prefix + "/detail", map);
         return view;
     }
 
-//    @GetMapping(value = "/h5/gzcxindex")
+    //    @GetMapping(value = "/h5/gzcxindex")
     @GetMapping(value = "/h5/dzgzzhindex")
     public ModelAndView dzgzzhindex() {
-        ModelAndView view = new ModelAndView( prefix+"/index");
+        ModelAndView view = new ModelAndView(prefix + "/index");
         return view;
     }
 
     @GetMapping(value = "/h5/index2")
     public ModelAndView index2() {
-        ModelAndView view = new ModelAndView( prefix+"/index");
+        ModelAndView view = new ModelAndView(prefix + "/index");
         return view;
     }
 
 
-
     @GetMapping(value = "/h5/echarts")
-    public ModelAndView echarts(HttpServletRequest request,String accessToken, ModelMap map) throws UnsupportedEncodingException {
-
-        byte[] aa = "恩施市博文学校".getBytes("utf-8");
-        for(int i=0; i<aa.length;i++)
-        {
-
-            System.out.print(String.format("%02x",aa[i]).toUpperCase());
-        }
-        System.err.println("===");
-
-        accessToken=iTokenService.getWebTokenByAppToken(accessToken).getData();
+    public ModelAndView echarts(HttpServletRequest request, String accessToken, ModelMap map)  {
+        accessToken = iTokenService.getWebTokenByAppToken(accessToken).getData();
         boolean isMoblie = PcUtils.JudgeIsMoblie(request);
-        if(StringUtils.isBlank(accessToken)) {
+        if (StringUtils.isBlank(accessToken)) {
             return new ModelAndView(prefix + "/error");
         }
         rpcLoginService.login(accessToken);
 
-        if(isMoblie){
-            ModelAndView view = new ModelAndView( "/h5/echartsh5");
+        if (isMoblie) {
+            ModelAndView view = new ModelAndView("/h5/echartsh5");
             return view;
         }
-        ModelAndView view = new ModelAndView( "/h5/echarts");
+        ModelAndView view = new ModelAndView("/h5/echarts");
         return view;
     }
 
 
     @GetMapping(value = "/h5/echartsh5")
-    public ModelAndView echartsh5(HttpServletRequest request,String accessToken, ModelMap map) {
-        accessToken=iTokenService.getWebTokenByAppToken(accessToken).getData();
-        if(StringUtils.isBlank(accessToken)) {
+    public ModelAndView echartsh5(HttpServletRequest request, String accessToken, ModelMap map) {
+        accessToken = iTokenService.getWebTokenByAppToken(accessToken).getData();
+        if (StringUtils.isBlank(accessToken)) {
             return new ModelAndView(prefix + "/error");
         }
         rpcLoginService.login(accessToken);
-        map.put("h5","h5");
-            ModelAndView view = new ModelAndView( "/h5/echartsh5",map);
-            return view;
+        map.put("h5", "h5");
+        ModelAndView view = new ModelAndView("/h5/echartsh5", map);
+        return view;
     }
 
     @GetMapping(value = "/h5/phone2023")
-    public ModelAndView phone2023() {
-        ModelAndView view = new ModelAndView( "/h5/phone2023");
+    public ModelAndView phone2023(String position,ModelMap map) {
+        map.put("position",position);
+        ModelAndView view = new ModelAndView("/h5/phone2023", map);
         return view;
     }
+
+
     @GetMapping(value = "/h5/phone2024")
     public ModelAndView phone2024() {
-        ModelAndView view = new ModelAndView( "/h5/phone2024");
+        ModelAndView view = new ModelAndView("/h5/phone2024");
         return view;
     }
 
 
     @GetMapping(value = "/h5/dwjw2024")
     public ModelAndView dwjw2024() {
-        ModelAndView view = new ModelAndView( "/h5/dwjw2024");
+        ModelAndView view = new ModelAndView("/h5/dwjw2024");
         return view;
     }
 
